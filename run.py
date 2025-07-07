b81bd50bed835096b4eae85546b91e672ce044d8
Runze Yang <78414187+runze1223@users.noreply.github.com>
2025-07-07 17:16:32 +0800

    Add files via upload

--- run.py
+++ run.py
@@ -9,19 +9,15 @@ from exp.exp_short_term_forecasting import Exp_Short_Term_Forecast
 import random
 import numpy as np
 
-print(torch.version.cuda)
-
-# fix_seed = 2021
-# random.seed(fix_seed)
-# torch.manual_seed(fix_seed)
-# np.random.seed(fix_seed)
+fix_seed = 2021
+random.seed(fix_seed)
+torch.manual_seed(fix_seed)
+np.random.seed(fix_seed)
 
 
 
 parser = argparse.ArgumentParser(description='TimeMixer')
-
-
-parser.add_argument('--seed', type=int, default=2021, help='for TimesBlock')
+# parser.add_argument('--seed', type=int, default=2021, help='for TimesBlock')
 # basic config
 parser.add_argument('--task_name', type=str, required=True, default='long_term_forecast',
                     help='task name, options:[long_term_forecast, short_term_forecast, imputation, classification, anomaly_detection]')
@@ -29,7 +25,6 @@ parser.add_argument('--is_training', type=int, required=True, default=1, help='s
 parser.add_argument('--model_id', type=str, required=True, default='test', help='model id')
 parser.add_argument('--model', type=str, required=True, default='Autoformer',
                     help='model name, options: [Autoformer, Transformer, TimesNet]')
-
 # data loader
 parser.add_argument('--data', type=str, required=True, default='ETTm1', help='dataset type')
 parser.add_argument('--root_path', type=str, default='./data/ETT/', help='root path of the data file')
@@ -40,34 +35,20 @@ parser.add_argument('--target', type=str, default='OT', help='target feature in 
 parser.add_argument('--freq', type=str, default='h',
                     help='freq for time features encoding, options:[s:secondly, t:minutely, h:hourly, d:daily, b:business days, w:weekly, m:monthly], you can also use more detailed freq like 15min or 3h')
 parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='location of model checkpoints')
-
 # forecasting task
 parser.add_argument('--seq_len', type=int, default=96, help='input sequence length')
 parser.add_argument('--label_len', type=int, default=0, help='start token length')
 parser.add_argument('--pred_len', type=int, default=96, help='prediction sequence length')
 parser.add_argument('--seasonal_patterns', type=str, default='Monthly', help='subset for M4')
 parser.add_argument('--inverse', action='store_true', help='inverse output data', default=False)
-
-
-# model define
+#other model 
 parser.add_argument('--top_k', type=int, default=5, help='for TimesBlock')
 parser.add_argument('--num_kernels', type=int, default=6, help='for Inception')
-parser.add_argument('--enc_in', type=int, default=7, help='encoder input size')
-parser.add_argument('--dec_in', type=int, default=7, help='decoder input size')
-parser.add_argument('--c_out', type=int, default=7, help='output size')
-parser.add_argument('--d_model', type=int, default=16, help='dimension of model')
-parser.add_argument('--n_heads', type=int, default=4, help='num of heads')
-parser.add_argument('--e_layers', type=int, default=2, help='num of encoder layers')
-parser.add_argument('--d_layers', type=int, default=1, help='num of decoder layers')
-parser.add_argument('--d_ff', type=int, default=32, help='dimension of fcn')
 parser.add_argument('--moving_avg', type=int, default=25, help='window size of moving average')
 parser.add_argument('--factor', type=int, default=1, help='attn factor')
 parser.add_argument('--distil', action='store_false',
                     help='whether to use distilling in encoder, using this argument means not using distilling',
                     default=True)
-parser.add_argument('--dropout', type=float, default=0.1, help='dropout')
-parser.add_argument('--embed', type=str, default='timestamp',
-                    help='time features encoding, options:[timeF, fixed, learned]')
 parser.add_argument('--activation', type=str, default='gelu', help='activation')
 parser.add_argument('--output_attention', action='store_true', help='whether to output attention in ecoder')
 parser.add_argument('--channel_independence', type=int, default=1,
@@ -81,14 +62,26 @@ parser.add_argument('--down_sampling_method', type=str, default='avg',
                     help='down sampling method, only support avg, max, conv')
 parser.add_argument('--use_future_temporal_feature', type=int, default=0,
                     help='whether to use future_temporal_feature; True 1 False 0')
-
+parser.add_argument('--d_layers', type=int, default=1, help='num of decoder layers')
+parser.add_argument('--patch_len', type=int, default=16, help='patch length')
+parser.add_argument('--stride', type=int, default=8, help='stride')
+parser.add_argument('--padding_patch', default='end', help='None: None; end: padding on the end')
+parser.add_argument('--decomposition', type=int, default=0, help='decomposition; True 1 False 0')
+parser.add_argument('--kernel_size', type=int, default=24, help='decomposition-kernel')
 # imputation task
 parser.add_argument('--mask_rate', type=float, default=0.25, help='mask ratio')
-
 # anomaly detection task
 parser.add_argument('--anomaly_ratio', type=float, default=0.25, help='prior anomaly ratio (%)')
-
-# optimization
+# de-stationary projector params
+parser.add_argument('--p_hidden_dims', type=int, nargs='+', default=[128, 128],
+                    help='hidden layer dimensions of projector (List)')
+parser.add_argument('--p_hidden_layers', type=int, default=2, help='number of hidden layers in projector')
+# GPU
+parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
+parser.add_argument('--gpu', type=int, default=0, help='gpu')
+parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
+parser.add_argument('--devices', type=str, default='0,1', help='device ids of multile gpus')
+#optimization
 parser.add_argument('--num_workers', type=int, default=10, help='data loader num workers')
 parser.add_argument('--itr', type=int, default=1, help='experiments times')
 parser.add_argument('--train_epochs', type=int, default=100, help='train epochs')
@@ -101,75 +94,56 @@ parser.add_argument('--lradj', type=str, default='ST', help='adjust learning rat
 parser.add_argument('--pct_start', type=float, default=0.2, help='pct_start')
 parser.add_argument('--use_amp', action='store_true', help='use automatic mixed precision training', default=False)
 parser.add_argument('--comment', type=str, default='none', help='com')
-
-# GPU
-parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
-parser.add_argument('--gpu', type=int, default=0, help='gpu')
-parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
-parser.add_argument('--devices', type=str, default='0,1', help='device ids of multile gpus')
-
-# de-stationary projector params
-parser.add_argument('--p_hidden_dims', type=int, nargs='+', default=[128, 128],
-                    help='hidden layer dimensions of projector (List)')
-parser.add_argument('--p_hidden_layers', type=int, default=2, help='number of hidden layers in projector')
-
-#timestamp 
+#FBM-S Model hyperparameter
+parser.add_argument('--enc_in', type=int, default=7, help='encoder input size')
+parser.add_argument('--dec_in', type=int, default=7, help='decoder input size')
+parser.add_argument('--c_out', type=int, default=7, help='output size')
+#timestamp
+parser.add_argument('--embed', type=str, default='timestamp',
+                    help='time features encoding, options:[timeF, fixed, learned]')  
 parser.add_argument('--individual_embed', type=int, default=0, help='individual head; True 1 False 0')
 parser.add_argument('--embedding',nargs="+", default=[0,3],help='timestamp embedding:{ 0: Year effect 1: Week-day effect 2: Week effect 3: Day effect ',type=int)
-parser.add_argument('--beta', type=float, default=0, help='control the prior and posterior information')
-parser.add_argument('--alpha_0', type=float, default=0, help='control the KL divergence for hierarchicail timestamp block')
-parser.add_argument('--alpha_1', type=float, default=0, help='control the KL divergence for harmonic seasoanl block')
-#
-parser.add_argument('--patch_num', type=int, default=14, help='patch number') 
-#
-parser.add_argument('--cut1', type=int, default=96, help='interaction_back_window')
-parser.add_argument('--cut2', type=int, default=24, help='interaction_forecst_window')
-parser.add_argument('--hidden1', type=int, default=1440, help='MLP_backbone_hiiden1')
-parser.add_argument('--hidden2', type=int, default=720, help='MLP_backbone_hiiden2')
-parser.add_argument('--dropout_total', type=float, default=0, help='final_dropout')
-parser.add_argument('--dropout_total2', type=float, default=0, help='final_dropout')
-parser.add_argument('--linear', type=int, default=1, help='1: linear; 0: MLP')
-parser.add_argument('--multiscale', type=int, default=0, help='0:no down sampling; 1:down sampling once; 2: down sampling twice')
+parser.add_argument('--beta', type=float, default=1, help='control the prior and posterior information')
+#Model block
 parser.add_argument('--timestamp', type=int, default=0, help='timestamp; True 1 False 0')
 parser.add_argument('--seasonal', type=int, default=1, help='seasonal_base_backbone; True 1 False 0')
 parser.add_argument('--trend', type=int, default=1, help='trend_base_backbone; True 1 False 0')
 parser.add_argument('--interaction', type=int, default=0, help='Interaction_backbone; True 1 False 0')
 parser.add_argument('--self_backbone', type=str, default='MLP', help='PatchTST:then use the transformer_backbone else use MLP_backbone')
-parser.add_argument('--d_model2', type=int, default=512, help='dimension of iTransformer backbone')
-parser.add_argument('--drop_initial', type=int, default=0, help='whether downsample the original granuarity')
-parser.add_argument('--centralization', type=int, default=0, help='whether normalize the patch')
-parser.add_argument('--patch', type=int, default=1, help='whether patch the MLP backbone')
-parser.add_argument('--channel_mask', type=int, default=0, help='whether patch the MLP backbone')
-
-parser.add_argument('--individual', type=int, default=0, help='individual head; True 1 False 0')
-parser.add_argument('--fc_dropout', type=float, default=0.05, help='fully connected dropout')
-parser.add_argument('--head_dropout', type=float, default=0.0, help='head dropout')
-parser.add_argument('--patch_len', type=int, default=16, help='patch length')
-parser.add_argument('--stride', type=int, default=8, help='stride')
-parser.add_argument('--padding_patch', default='end', help='None: None; end: padding on the end')
+#Standarization
 parser.add_argument('--revin', type=int, default=1, help='RevIN; True 1 False 0')
 parser.add_argument('--affine', type=int, default=0, help='RevIN-affine; True 1 False 0')
 parser.add_argument('--subtract_last', type=int, default=0, help='0: subtract mean; 1: subtract last')
-parser.add_argument('--decomposition', type=int, default=0, help='decomposition; True 1 False 0')
-parser.add_argument('--kernel_size', type=int, default=24, help='decomposition-kernel')
+#Multi-scale
+parser.add_argument('--multiscale', type=int, default=0, help='0:no down sampling; 1:down sampling once; 2: down sampling twice')
+parser.add_argument('--drop_initial', type=int, default=0, help='whether downsample the original granuarity')
+#Trend(MLP)
+parser.add_argument('--cut1', type=int, default=24, help='interaction_back_window')
+parser.add_argument('--cut2', type=int, default=24, help='interaction_forecst_window')
+parser.add_argument('--hidden1', type=int, default=256, help='MLP_backbone_hiiden1')
+parser.add_argument('--hidden2', type=int, default=1440, help='MLP_backbone_hiiden2')
+parser.add_argument('--dropout', type=float, default=0.15, help='dropout')
+parser.add_argument('--dropout_total', type=float, default=0, help='final_dropout')
+parser.add_argument('--dropout_total2', type=float, default=0, help='final_dropout')
+parser.add_argument('--patch', type=int, default=1, help='whether patch the MLP backbone')
+parser.add_argument('--linear', type=int, default=0, help='1: linear; 0: MLP')
+parser.add_argument('--centralization', type=int, default=0, help='whether normalize the patch in trend block')
+#Trend(PatchTST)
+parser.add_argument('--patch_num', type=int, default=14, help='patch number, this must be divisible by input sequence length')
+parser.add_argument('--d_model', type=int, default=128, help='dimension of model')
+parser.add_argument('--d_ff', type=int, default=256, help='dimension of fcn')
+parser.add_argument('--n_heads', type=int, default=16, help='num of heads')
+parser.add_argument('--e_layers', type=int, default=3, help='num of encoder layers')
+parser.add_argument('--individual', type=int, default=0, help='individual head; True 1 False 0')
+parser.add_argument('--fc_dropout', type=float, default=0.2, help='fully connected dropout')
+parser.add_argument('--head_dropout', type=float, default=0.0, help='head dropout') 
+#Interaction
+parser.add_argument('--d_model2', type=int, default=512, help='dimension of iTransformer backbone')
+parser.add_argument('--channel_mask', type=int, default=0, help='whether patch the MLP backbone')
 parser.add_argument('--dropout2', type=float, default=0.15, help='dropout')
 
-
-parser.add_argument('--noisy_gating', type=int, default=1, help='0: subtract mean; 1: subtract last')
-parser.add_argument('--num_experts', type=int, default=1, help='decomposition; True 1 False 0')
-parser.add_argument('--k', type=int, default=1, help='decomposition-kernel')
-parser.add_argument('--CI', type=int, default=1, help='0: subtract mean; 1: subtract last')
-parser.add_argument('--hidden_size', type=int, default=256, help='0: subtract mean; 1: subtract last')
-
-
 args = parser.parse_args()
-fix_seed = args.seed
-random.seed(fix_seed)
-torch.manual_seed(fix_seed)
-np.random.seed(fix_seed)
 args.use_gpu = True if torch.cuda.is_available() and args.use_gpu else False
-
-
 
 if args.use_gpu and args.use_multi_gpu:
     args.devices = args.devices.replace(' ', '')
@@ -179,7 +153,6 @@ if args.use_gpu and args.use_multi_gpu:
 
 print('Args in experiment:')
 print(args)
-
 
 
 if args.task_name == 'long_term_forecast':
